4ba9b96842294ca49098093d30d1ac2ee6e53a1b
reisryan <211036132@aluno.unb.br>
2025-01-31 16:42:16 -0300

    adição de install_dependencies.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# projeto_mp_24.2
+# Projeto Final de Métodos de Programação
+## Semestre 2024.2 - Universidade de Brasília
+### Departamento de Ciência da Computação
+
+#### Para instalar, use: python install_dependencies.py
---
+++ install_dependencies.py
@@ -0,0 +1,20 @@
+import sys
+import subprocess
+
+def install_packages():
+    packages = [
+        "pycodestyle", "pytest", "django", "pyflakes", "coverage", "sphinx"
+    ]
+    
+    try:
+        # Atualiza o pip antes de instalar os pacotes
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+        
+        # Instala os pacotes necessários
+        subprocess.check_call([sys.executable, "-m", "pip", "install"] + packages)
+        print("\nTodas as bibliotecas foram instaladas com sucesso!")
+    except subprocess.CalledProcessError:
+        print("Erro ao instalar as bibliotecas. Verifique sua conexão com a internet e tente novamente.")
+
+if __name__ == "__main__":
+    install_packages()
